0aaed0e3fb605b781a241a0a22da1c2aae19f7bc
IsaiVids <isaivids2020@gmail.com>
2022-04-22 21:10:42 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import {FaGithub, FaInstagram, FaWhatsapp, FaTwitter} from 'react-icons/fa'
 
 function Footer() {
   return (
-    <Footer className='footer'>
+    <div className='footer'>
         <div className="footer-icons">
             <FaGithub/><FaInstagram/>
             <FaWhatsapp/><FaTwitter/>
         </div>
       <p class="copyright">Magic Moments © 2022</p>
-    </Footer>
+    </div>
   )
 }
 
